9a3b99142327aa327a4522b0699357977ca57090
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-10-28 18:37:31 +0500

    Add Components in Project 02

--- project02/App.tsx
+++ project02/App.tsx
@@ -1,11 +1,13 @@
 import { Text, SafeAreaView, ScrollView } from 'react-native';
 import React from 'react';
+import FlatCards from './components/FlatCards';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <Text>App</Text>
+        <FlatCards />
       </ScrollView>
     </SafeAreaView>
   );
