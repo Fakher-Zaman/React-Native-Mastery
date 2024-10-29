b16699397ae4577d81a107d4e3e53d3678063450
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-10-29 18:27:20 +0500

    Add Cards with UI display

--- project02/App.tsx
+++ project02/App.tsx
@@ -1,13 +1,14 @@
-import { Text, SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>App</Text>
         <FlatCards />
+        <ElevatedCards />
       </ScrollView>
     </SafeAreaView>
   );
--- project02/components/FlatCards.tsx
+++ project02/components/FlatCards.tsx
@@ -3,10 +3,62 @@ import React from 'react';
 
 export default function FlatCards() {
     return (
-        <View>
-            <Text>FlatCards</Text>
+        <View style={styles.main}>
+            <Text style={styles.headingText}>Flat Cards</Text>
+            <View style={styles.container}>
+                <View style={[styles.card, styles.cardOne]}>
+                    <Text>Red</Text>
+                </View>
+                <View style={[styles.card, styles.cardTwo]}>
+                    <Text>Green</Text>
+                </View>
+                <View style={[styles.card, styles.cardThree]}>
+                    <Text>Blue</Text>
+                </View>
+                <View style={[styles.card, styles.cardFour]}>
+                    <Text>Yellow</Text>
+                </View>
+            </View>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        alignItems: 'flex-start',
+        marginTop: 16,
+        padding: 4,
+    },
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8,
+        paddingVertical: 16,
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    card: {
+        flex: 1,
+        width: '20%',
+        height: 100,
+        borderRadius: 4,
+        margin: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cardOne: {
+        backgroundColor: '#EF5354',
+    },
+    cardTwo: {
+        backgroundColor: '#50DBB4',
+    },
+    cardThree: {
+        backgroundColor: '#5DA3FA',
+    },
+    cardFour: {
+        backgroundColor: '#FFFF8D',
+    },
+});
