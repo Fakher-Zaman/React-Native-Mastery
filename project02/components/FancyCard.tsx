2eeceb64db3a01ad57c0d541898fea724c836d95
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-10-30 11:57:50 +0500

    Add FlatCards and FancyCard component in the second project

---
+++ project02/components/FancyCard.tsx
@@ -0,0 +1,72 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function FancyCard() {
+    return (
+        <View>
+            <Text style={styles.headingText}>Trending Assets</Text>
+            <View style={[styles.card, styles.cardElevated]}>
+                <Image
+                    style={styles.cardImage} 
+                    source={{
+                        uri: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQQZA8s3i80S9sJnQwPMBucnYOuPjOsPWuuWKP272agfS60vRU_0o1Vzv_6W03OySwua1OyWOjO2wlK9hVL2lzOgQ'
+                    }}
+                />
+                <View style={styles.cardBody}>
+                    <Text style={styles.cardTitle}>Hawa Mahal</Text>
+                    <Text style={styles.cardLabel}>Pink City, jaipur</Text>
+                    <Text style={styles.cardDescription}>The Hawa Mahal is a palace in the city of Jaipur, India. Built from red and pink sandstone, it is on the edge of the City Palace.</Text>
+                    <Text style={styles.cardFooter}>12 mins away</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8,
+    },
+    card: {
+        height: 360,
+        borderRadius: 6,
+        marginVertical: 12,
+        marginHorizontal: 16,
+    },
+    cardElevated: {
+        backgroundColor: '#ddfcf7',
+        elevation: 4,
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: '#333',
+    },
+    cardImage: {
+        height: 200,
+        marginBottom: 8,
+        borderTopLeftRadius: 6,
+        borderTopRightRadius: 6,
+    },
+    cardBody: {
+        flex: 1,
+        flexGrow: 1,
+        paddingHorizontal: 12,
+    },
+    cardTitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 4,
+    },
+    cardLabel: {
+        fontSize: 14,
+        marginBottom: 6,
+    },
+    cardDescription: {
+        fontSize: 12,
+        color: '#758283',
+        marginBottom: 12,
+        marginTop: 6,
+        flexShrink: 1,
+    },
+    cardFooter: { },
+});
