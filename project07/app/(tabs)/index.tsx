bc0c851ffdda6106c59cef1cdfb67a4dd76e99f8
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-11-05 15:47:44 +0500

    Snackbar issue occured

--- project07/app/(tabs)/index.tsx
+++ project07/app/(tabs)/index.tsx
@@ -1,10 +1,92 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import Snackbar from 'react-native-snackbar';
+import Icons from '@/components/Icons';
+import { useState } from 'react';
 
-export default function HomeScreen() {
+export default function HomeScreen(): JSX.Element {
+  const [isCross, serIsCross] = useState<boolean>(false);
+  const [gameWinner, setGameWinner] = useState<string>('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+
+  const reloadGame = () => {
+    setGameState(new Array(9).fill('empty', 0, 9));
+    setGameWinner('');
+    serIsCross(false);
+  };
+
+  const checkIsWinner = () => {
+    //  checking  winner of the game
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== 'empty'
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[3] !== 'empty' &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} won the game! 🥳`);
+    } else if (
+      gameState[6] !== 'empty' &&
+      gameState[6] === gameState[7] &&
+      gameState[7] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[1] !== 'empty' &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game... ⌛️');
+    }
+  }
+
+  // const onChangeItem = (itemNumber: number) => {
+  //   if(gameWinner) {
+  //     return Snackbar.show({
+  //       text: gameWinner,
+  //       backgroundColor: '#000000',
+  //       duration: Snackbar.LENGTH_LONG,
+  //     })
+  //   }
+  // }
+
   return (
-    <View>
-      <Text>React Native Projects</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>React Native Game</Text> 
+      </View>
+    </SafeAreaView>
   );
 }
 
